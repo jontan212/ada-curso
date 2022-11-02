b8f1c023379aa086e8aa6e6ddf068d770f2edb27
Jonathan <jontan212yt@gmail.com>
2022-11-02 11:42:24 +0100

    ej2

--- EjResponsable/Ejercicio2/src/main.adb
+++ EjResponsable/Ejercicio2/src/main.adb
@@ -2,15 +2,24 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
   Salida : String (1 .. 9);
-  procedure Copiar_String (Sobrante : String) is
+  procedure Copiar_String (Sobrante : Character) is
     Variable : String (1 .. 5) := Get_Line;
   begin
-    Salida := Variable;
-    Put_Line (Variable'Length'Image);
+    if Salida'Length > Variable'Length then
 
-    Put_Line (Salida'Length'Image);
+      Salida (1 .. 5) := Variable;
+      for I in Salida'Range loop
+        if I > Variable'Length then
+          Salida (I) := Sobrante;
+        end if;
+      end loop;
+
+    else
+      Salida (1 .. 5) := Variable;
+    end if;
+    Put_Line (Salida);
   end Copiar_String;
 
 begin
-  Copiar_String ("*");
+  Copiar_String ('*');
 end Main;
